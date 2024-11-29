a0781f0539b433bd104efb02c04b99c3b938cfed
Pierre Arragain-Dehlinger <pierre@algorithma.fr>
2024-11-29 20:34:38 +0100

    feat(docker): create docker files and docker compose

--- frontend/weather-app/next.config.ts
+++ frontend/weather-app/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL || "http://localhost:3000",
+  },
   reactStrictMode: true,
   async rewrites() {
     return [
@@ -9,6 +12,9 @@ const nextConfig: NextConfig = {
         destination: `${process.env.BACKEND_URL}/:path*`, // URL de votre backend
       },
     ];
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
